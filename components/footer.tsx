dfbeba1e5ed55728c7cb1a431712564b1f8f0b31
olegmoshkovich <oleg.mosh@gmail.com>
2025-09-11 22:23:22 +0200

    change the alignemen of the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="w-full border-t border-gray-100 py-8 px-3 sm:px-5 mt-40 sm:mt-20">
       <div className="max-w-5xl mx-auto">
         
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ml-6">
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-6 ml-6">
           {/* Left Column - Company Information */}
           <div className="text-left">
             <div className="text-xs text-gray-500 space-y-1">
@@ -20,7 +20,7 @@ export function Footer() {
                   href="https://www.cloneit.at" 
                   target="_blank" 
                   rel="noopener noreferrer"
-                  className="text-blue-600 hover:text-blue-800 underline"
+                  className="text-grey-600 hover:text-blue-600 underline"
                 >
                   www.cloneit.at
                 </a>
@@ -35,8 +35,26 @@ export function Footer() {
               <div>Firmenbuchgericht: Graz</div>
               <div>UID-Nr.: ATU79501148</div>
               <div className="mt-3 space-y-1">
-                <div>CEO: Dipl. Ing. Paul Wegerer</div>
-                <div>CTO: Dipl. Ing. Liebhard Mattuschka</div>
+                <div>
+                  <a 
+                    href="https://www.linkedin.com/in/paul-wegerer-364248236/" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className="text-grey-600 hover:text-blue-600 underline"
+                  >
+                    CEO: Dipl. Ing. Paul Wegerer
+                  </a>
+                </div>
+                <div>
+                  <a 
+                    href="https://www.linkedin.com/in/liebhard-mattuschka-9b33b9239/" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className="text-grey-600 hover:text-blue-600 underline"
+                  >
+                    CTO: Dipl. Ing. Liebhard Mattuschka
+                  </a>
+                </div>
               </div>
               <div className="mt-3">
                 Mitglied: Wirtschaftskammer Steiermark (WKO)
