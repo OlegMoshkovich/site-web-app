515aca56b225d14ef3be66d9c2bd9b16cc1f4faa
olegmoshkovich <oleg.mosh@gmail.com>
2025-10-06 16:54:41 +0200

    change the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Clone:it Simple Site",
+  title: "clone:it Simple Site",
   description: "Site web app",
 };
 
