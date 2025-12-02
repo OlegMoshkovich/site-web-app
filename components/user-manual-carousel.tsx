62215953c8225f187cf6b72d39dee958de126930
olegmoshkovich <oleg.mosh@gmail.com>
2025-12-02 13:47:30 +0100

    change order of the images

--- components/user-manual-carousel.tsx
+++ components/user-manual-carousel.tsx
@@ -29,10 +29,11 @@ export function UserManualCarousel({
 
   // User manual images
   const images = [
-    { src: "/userManual/1.png", alt: "User Manual Page 1" },
-    { src: "/userManual/2.png", alt: "User Manual Page 2" },
+
     { src: "/userManual/3.png", alt: "User Manual Page 3" },
     { src: "/userManual/4.png", alt: "User Manual Page 4" },
+    { src: "/userManual/2.png", alt: "User Manual Page 2" },
+
     { src: "/userManual/5.png", alt: "User Manual Page 5" },
   ];
 
