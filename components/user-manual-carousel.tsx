19409403d9ab82ebd21199029d16616cf3692f91
olegmoshkovich <oleg.mosh@gmail.com>
2025-12-04 21:46:53 +0100

    change the order of images in the user manual

--- components/user-manual-carousel.tsx
+++ components/user-manual-carousel.tsx
@@ -39,8 +39,8 @@ export function UserManualCarousel({
     { src: "/userManual/2.png", alt: "User Manual Page 2" },
     { src: "/userManual/3.png", alt: "User Manual Page 3" },
     { src: "/userManual/4.png", alt: "User Manual Page 4" },
-    { src: "/userManual/5.png", alt: "User Manual Page 5" },
-    { src: "/userManual/6.png", alt: "User Manual Page 6" },
+    { src: "/userManual/6.png", alt: "User Manual Page 5" },
+    { src: "/userManual/5.png", alt: "User Manual Page 6" },
   ];
 
   // Function to update URL with current slide
