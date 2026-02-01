870bb32f3bae97fdfa46051968b421a0156748f3
olegmoshkovich <oleg.mosh@gmail.com>
2026-02-01 16:54:58 +0100

    fix the manual input to the filter

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="de" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
