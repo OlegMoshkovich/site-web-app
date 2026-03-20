b42b20af04213507839c476a9a081dff691fc224
olegmoshkovich <oleg.mosh@gmail.com>
2026-03-20 11:43:37 +0100

    add models

--- app/model/custom/page.tsx
+++ app/model/custom/page.tsx
@@ -5,11 +5,9 @@ import Link from "next/link";
 import { AuthButtonClient } from "@/components/auth-button-client";
 import { getNavbarClasses } from "@/lib/layout-constants";
 
-export default function ModelPage() {
-
+export default function ModelCustomPage() {
   return (
     <div className="h-screen w-screen overflow-hidden flex flex-col">
-      {/* Top navigation bar matching non-signed-in state from index page */}
       <nav className={getNavbarClasses().container}>
         <div className={getNavbarClasses().content}>
           <div className="flex items-center gap-2">
@@ -23,19 +21,16 @@ export default function ModelPage() {
               />
             </Link>
           </div>
-
           <div className="flex items-center gap-2">
-            {/* AuthButton */}
             <AuthButtonClient />
           </div>
         </div>
       </nav>
 
-      {/* Full-window Speckle project iframe */}
       <div className="flex-1 w-full">
         <iframe
           title="Speckle"
-          src="https://app.speckle.systems/projects/f49286cce4"
+          src="https://app.speckle.systems/projects/f49286cce4/models/5ec1fa78d6?embedToken=95eaf20b48b8953f23bbb6c331dade5f529aeae43d#embed=%7B%22isEnabled%22%3Atrue%7D"
           className="w-full h-full border-0"
           allow="fullscreen"
         />
